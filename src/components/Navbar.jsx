c64c2d647179f2c1f4579b6a014ea20982249873
Soham Chavan <chavan.soha@northeastern.edu>
2024-12-23 15:00:51 -0500

    nav bar created

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../styles/Navbar.css';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="logo">MyPortfolio</div>
+      <ul className="nav-links">
+        <li><a href="#about">About</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
